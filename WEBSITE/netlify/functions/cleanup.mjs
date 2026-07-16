25a0ee640edf1102a68520b309309efc28864e73
TechnoWarriorrrTTV <moeloden@gmail.com>
2026-07-16 08:16:56 -0400

    Stop tracking and ignore WEBSITE folder

--- WEBSITE/netlify/functions/cleanup.mjs
+++
@@ -1,9 +0,0 @@
-import { cleanupExpired, transaction } from '../lib/db.mjs'
-import { json, route } from '../lib/http.mjs'
-
-export default route(async () => {
-  await transaction(cleanupExpired)
-  return json({ status: 'success' })
-})
-
-export const config = { schedule: '@hourly' }
